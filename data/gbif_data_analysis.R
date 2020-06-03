b2991221c3465f0caea2641fd94d5fa4e0ddd0d6
fendabenda <brendathompson009@gmail.com>
2020-06-03 13:13:55 -0400

    GBIF data

---
+++ data/gbif_data_analysis.R
@@ -0,0 +1,12 @@
+setwd("/Users/brendathompson/Desktop/mylocalrepo")
+bees <- read.csv('canada_bee_dataset.csv')
+
+distinct(bees$species)
+
+x<- length(unique(bees$species))
+x<- length(unique(bees$genus))
+
+unique(bees["species","genus"])
+
+options(max.print=100000)
+species_count = as.data.frame(table(bees$species, bees$genus))
