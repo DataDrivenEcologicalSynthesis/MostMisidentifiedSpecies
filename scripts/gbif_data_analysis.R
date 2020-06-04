d17d7f6254f77d790af7e6473dd2a90c45e51fb1
fendabenda <brendathompson009@gmail.com>
2020-06-04 10:09:34 -0400

    Adding operations to normalize API data
    In many cases, inputs for the 'species guess' and 'genus' variables did not correspond with one another. For example, where a species-level species guess was given, the input for the 'genus' variable was a family-level ID. These cases are dealt with here.

--- scripts/gbif_data_analysis.R
+++ scripts/gbif_data_analysis.R
@@ -44,7 +44,55 @@ species_genus_2 <- species_genus_2[!duplicated(species_genus_2$species),]
 genuscount_2 <- as.data.frame(table(species_genus_2$genus))
 
 
-### Join bees table to genuscount_2 based on genus name
+# Now we need to deal with the problematic species names in data_api, but we'll identify them first here: 
+
+problematic_genera <- new_bees[is.na(new_bees$species_count),]
+
+# Now we have repeats of problematic genera, so now I'll create a subset with only unique values.
+
+problematic_genera <- problematic_genera[!duplicated(problematic_genera$genus),]
+
+# Now with our list, I will re-assign new genus names to the problematic ones, that way they can be identified from the GBIF data.
+# Apinae,Xylocopinae, and Meliponini are subfamilies and so identifications at the rank subfamily do not qualify for this genus-species count analysis
+# Anthophorini,Epeolini,Bombini,Apini, and Eucerini are tribes and so are also excluded.
+# I use new_data_api here which is just a subset of the api data, so feel free to use whatever datatable you want here.
+new_data_api$genus[new_data_api$genus=='Alpinobombus'] <- 'Bombus'
+new_data_api$genus[new_data_api$genus=='bombus'] <- 'Bombus'
+new_data_api$genus[new_data_api$genus=='Psithyrus'] <- 'Bombus'
+new_data_api$genus[new_data_api$genus=='Pyrobombus'] <- 'Bombus'
+new_data_api$genus[new_data_api$genus=='Zadontomerus'] <- 'Ceratina'
+new_data_api$genus[new_data_api$genus=='Thoracobombus'] <- 'Bombus'
+new_data_api$genus[new_data_api$genus=='Synhalonia'] <- 'Eucera'
+new_data_api$genus[new_data_api$genus=='Bombias'] <- 'Bombus'
+new_data_api$genus[new_data_api$genus=='Eumelissodes'] <- 'Melissodes'
+new_data_api$genus[new_data_api$genus=='Cullumanobombus'] <- 'Bombus'
+new_data_api$genus[new_data_api$genus=='Ceratinidia'] <- 'Ceratina'
+
+# Dealing with cases where a species level (or genus level) ID is reached, but the genus ID in new_data_api is subfamily or tribe. 
+# Apinae, Anthophorini,Epeolini,Eucerini, Apidae, and Meliponini are excluded because the species guesses made were not at the genus level or lower
+
+# Carpenter bees, which is the species guess for all observations with a genus input of 'Xylocopinae', are actually of genus Xylocopa
+new_data_api$genus[new_data_api$genus=='Xylocopinae'] <- 'Xylocopa'
+
+#There is only a single living genus of the tribe 'bombini', which is bombus, so this is an easy substitution
+new_data_api$genus[new_data_api$genus=='Bombini'] <- 'Bombus'
+
+# There is a single genus in the tribe Apini, which is Apis
+new_data_api$genus[new_data_api$genus=='Apini'] <- 'Apis'
+
+# Dealing with exceptions #
+# In some cases, a species guess does not reflect the genus ID given, so this will be examined more closely in the remaining problematic genera. 
+
+# Apidae
+new_data_api$genus[new_data_api$species_guess =='Great Basin Bumble Bee'] <- 'Bombus'
+new_data_api$genus[new_data_api$species_guess =='Common Eastern Bumble Bee'] <- 'Bombus'
+new_data_api$genus[new_data_api$species_guess =='Bumble Bee']<-'Bombus'
+new_data_api$genus[new_data_api$species_guess =='Eastern Carpenter Bee']<-'Xylocopa'
+new_data_api$genus[new_data_api$species_guess =='Carpenter Bee']<-'Xylocopa'
+new_data_api$genus[new_data_api$species_guess =='Carpenter bee']<-'Xylocopa'
+
+### Joining API data to the species per genus data ### 
+# Now that the tables have been normalized, joins can be made.
 
 genuscount_2 <- genuscount_2 %>% 
   rename(genus=Var1,
@@ -56,26 +104,6 @@ genuscount_2 <- genuscount_2 %>%
 
 new_bees <- left_join(new_data_api, genuscount_2)
 
-# Now we need to deal with the problematic species names in data_api, but we'll identify them first here: 
 
-problematic_genera <- new_bees[is.na(new_bees$species_count),]
 
-# Now we have repeats of problematic genera, so now I'll create a subset with only unique values.
-
-problematic_genera <- problematic_genera[!duplicated(problematic_genera$genus),]
-
-# Now with this list, we can re-assign new genus names to the problematic ones, that way they can be identified from the GBIF data. This can only be done for observations that have a higher-level genus rank (for example, observations in the API where the genus ID is actually subgenus; in this case, I grabbed the genus associated with that subgenus. But, this can't be done for observations where, for the genus ID, a family name is given)  
-# Apinae,Xylocopinae, and Meliponini are subfamilies and so identifications at the rank subfamily do not qualify for this genus-species count analysis
-# Anthophorini,Epeolini,Bombini,Apini, and Eucerini are tribes and so are also excluded.
-# I use new_data_api here which is just a subset of the api data, so feel free to use whatever datatable you want here.
-new_data_api$genus[new_data_api$genus=='bombus'] <- 'Bombus'
-new_data_api$genus[new_data_api$genus=='Psithyrus'] <- 'Bombus'
-new_data_api$genus[new_data_api$genus=='Pyrobombus'] <- 'Bombus'
-new_data_api$genus[new_data_api$genus=='Zadontomerus'] <- 'Ceratina'
-new_data_api$genus[new_data_api$genus=='Thoracobombus'] <- 'Bombus'
-new_data_api$genus[new_data_api$genus=='Synhalonia'] <- 'Eucera'
-new_data_api$genus[new_data_api$genus=='Bombias'] <- 'Bombus'
-new_data_api$genus[new_data_api$genus=='Eumelissodes'] <- 'Melissodes'
-new_data_api$genus[new_data_api$genus=='Cullumanobombus'] <- 'Bombus'
-new_data_api$genus[new_data_api$genus=='Ceratinidia'] <- 'Ceratina'
 
